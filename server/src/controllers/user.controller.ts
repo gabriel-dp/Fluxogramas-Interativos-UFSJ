bac47dc8a3d72565028ce12e51fce5f296a6c797
gabriel-meira <gabriel.meira@exati.com.br>
2025-03-02 19:31:28 -0300

    :wrench: Improve user and auth endpoints

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
+import { hash } from "bcryptjs";
 
 import prisma from "@/lib/prisma";
+import { AuthRequest, requireAdmin } from "@/middlewares/security.middleware";
 
-async function readAll(_: Request, res: Response) {
+async function readMany(_: Request, res: Response) {
 	try {
 		const allUsers = await prisma.user.findMany({
 			omit: { password: true },
@@ -22,7 +24,7 @@ async function readOne(req: Request, res: Response) {
 			omit: { password: true },
 		});
 
-		if (!user) res.sendStatus(404);
+		if (!user) return res.sendStatus(404);
 
 		res.status(200).json({ user });
 	} catch (error) {
@@ -30,9 +32,83 @@ async function readOne(req: Request, res: Response) {
 	}
 }
 
+async function createOne(req: AuthRequest, res: Response) {
+	const { login, password, ...data } = req.body;
+
+	if (data.isAdmin) requireAdmin(req, res, () => undefined); // Only admins can create other admins
+
+	try {
+		const existingUser = await prisma.user.findUnique({ where: { login } });
+		if (existingUser) return res.sendStatus(400);
+
+		const passwordHash = await hash(password, 10);
+		await prisma.user.create({
+			data: {
+				login,
+				password: passwordHash,
+				isAdmin: data.isAdmin,
+			},
+		});
+
+		return res.sendStatus(201);
+	} catch (error) {
+		return res.sendStatus(500);
+	}
+}
+
+async function updateOne(req: AuthRequest, res: Response) {
+	const id = parseInt(req.params.id, 10);
+	const { login, password, ...data } = req.body;
+
+	if (req.user?.id != id) requireAdmin(req, res, () => undefined); // Only admins can modify data from other users
+	if (data.isAdmin) requireAdmin(req, res, () => undefined); // Only admins can create other admins
+
+	try {
+		const existingUser = await prisma.user.findFirst({
+			where: {
+				login,
+				NOT: { id },
+			},
+		});
+		if (existingUser) return res.sendStatus(400);
+
+		const passwordHash = await hash(password, 10);
+		await prisma.user.update({
+			data: {
+				login,
+				password: passwordHash,
+				...data,
+			},
+			where: { id },
+		});
+
+		res.sendStatus(200);
+	} catch (error) {
+		res.sendStatus(500);
+	}
+}
+
+async function deleteOne(req: Request, res: Response) {
+	const id = parseInt(req.params.id, 10);
+
+	try {
+		await prisma.user.delete({ where: { id } });
+
+		const existingUser = await prisma.user.findUnique({ where: { id } });
+		if (!existingUser) return res.sendStatus(404);
+
+		res.sendStatus(200);
+	} catch (error) {
+		res.sendStatus(500);
+	}
+}
+
 const UserController = {
-	readAll,
+	readMany,
 	readOne,
+	createOne,
+	updateOne,
+	deleteOne,
 };
 
 export default UserController;
