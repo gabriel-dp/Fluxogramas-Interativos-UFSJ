90044cff2d1fbf3b4aa1dddfd2f9eb4f52ce965f
gabriel-meira <gabriel.meira@exati.com.br>
2025-03-02 19:31:28 -0300

    :wrench: Improve user and auth endpoints

--- server/src/routers/auth.router.ts
+++ server/src/routers/auth.router.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import AuthController from "@/controllers/auth.controller";
 import { getAuth } from "@/middlewares/security.middleware";
+import { validateData } from "@/middlewares/data.middleware";
+import { userRegistrationSchema, userSignInSchema } from "@/models/user.model";
 
 const router = Router();
 
-router.post("/sign-in", AuthController.signIn);
-router.post("/register", getAuth, AuthController.register);
+router.post("/sign-in", validateData(userSignInSchema), AuthController.signIn);
+router.post("/register", validateData(userRegistrationSchema), getAuth, AuthController.register);
 
 export default router;
--- server/src/routers/user.router.ts
+++ server/src/routers/user.router.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 
 import UserController from "@/controllers/user.controller";
-import { requireAdmin } from "@/middlewares/security.middleware";
+import { getAuth, requireAdmin } from "@/middlewares/security.middleware";
+import { validateData, validateId } from "@/middlewares/data.middleware";
+import { userRegistrationSchema } from "@/models/user.model";
 
 const router = Router();
 
-router.use(requireAdmin);
+router.get("/", requireAdmin, UserController.readMany);
+router.post("/", requireAdmin, validateData(userRegistrationSchema), UserController.createOne);
 
-router.get("/", UserController.readAll);
-router.get("/:id", UserController.readOne);
+router.get("/:id", getAuth, validateId, UserController.readOne);
+router.put("/:id", getAuth, validateId, validateData(userRegistrationSchema), UserController.updateOne);
+router.delete("/:id", requireAdmin, validateId, UserController.deleteOne);
 
 export default router;
