ac108862bb09b1988cdfd1389adddf9ca722d8f4
gabriel-meira <gabriel.meira@exati.com.br>
2025-07-14 09:24:51 -0300

    :art: Reduce evaluation card transparency

--- client/src/components/FormEvaluationCard/styles.ts
+++ client/src/components/FormEvaluationCard/styles.ts
@@ -18,7 +18,7 @@ export const CardContainer = styled.div`
 	padding: 1.5rem;
 	border-radius: 0.5rem;
 
-	background: ${(props) => props.theme.white}99;
+	background: ${(props) => props.theme.white}CC;
 	border-radius: 16px;
 	box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
 	backdrop-filter: blur(5px);
