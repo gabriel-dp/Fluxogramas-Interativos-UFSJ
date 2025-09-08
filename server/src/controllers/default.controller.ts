392739d8b5410585407deb80af961386b0b4b0d3
gabriel-dp <gabriel.meira@exati.com.br>
2025-09-08 13:46:37 -0300

    :bug: Fix paths + remove unnecessary files on build

--- server/src/controllers/default.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-async function helloWorld(req: Request, res: Response) {
-	return res.status(200).send("Hello World!");
-}
-
-export default { helloWorld };
--- server/src/routers/default.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import DefaultController from "@/controllers/default.controller";
-
-const router = Router();
-
-router.get("/", DefaultController.helloWorld);
-
-export default router;
