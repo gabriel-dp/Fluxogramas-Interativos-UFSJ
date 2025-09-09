18db61dbcbe36d223e541468550d9791a3b9ecbd
gabriel-dp <gabriel.meira@exati.com.br>
2025-09-09 00:53:05 -0300

    :rocket: Server cors origin defined in a environment variable

--- server/src/configs/index.ts
+++ server/src/configs/index.ts
@@ -9,7 +9,7 @@ import databaseConfig from "./database.config";
 export default async function configApp(app: Express): Promise<void> {
 	app.use(express.json());
 	app.use(cookieParser());
-	app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+	app.use(cors({ origin: process.env.SERVER_CORS_ORIGIN, credentials: true }));
 	app.use(requestLog);
 	app.use(router);
 
